8619e93ab9c8db6c4e446ddd6720e032d499f1f2
saby00 <143467712+saby00@users.noreply.github.com>
2024-05-07 21:36:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,6 @@ async def get_user_for_genre(genre: str):
     result = UserForGenre(genre)
     return JSONResponse(content=result)
 
-@app.get("/developer_reviews_analysis/{developer_name}")
-async def get_developer_reviews(developer_name: str):
-    result = developer_reviews_analysis(developer_name)
-    return JSONResponse(content=result)
-
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
